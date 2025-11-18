f292866186fc23fb791a4d390000703a077fb61e
EuanHam <euanham05@gmail.com>
2025-11-17 20:06:43 -0500

    front end log in function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,41 @@ import InputField from './components/InputField'
 import Link from 'next/link';
 import TopBar from './components/TopBar';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
 export default function LogIn() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
+  const handleLogin = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch('/api/user/verify', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        localStorage.setItem('userId', data.id);
+        localStorage.setItem('isAdmin', data.admin);
+        
+        router.push('/pages/trainingLogsDashboard');
+      } else {
+        alert('Invalid email or password');
+      }
+    } catch (err) {
+      alert('Something went wrong.');
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <div>
@@ -18,25 +49,28 @@ export default function LogIn() {
         <div></div>
         <div className="flex flex-col w-full justify-center items-center max-w-md space-y-5 text-xs">
           <p className="text-[30px] font-bold">Login</p>
+
           <InputField
-            type="text"
+            type="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             placeholder="Email"
           />
 
           <InputField
-            type="text"
+            type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             placeholder="Password"
           />
 
-          <Link href="/pages/trainingLogsDashboard" className="w-full pt-4">
-            <button className="bg-red-700 text-white w-full text-lg rounded-xl p-3 mt-2 mb-4 font-bold text-xl">
-              Log in
-            </button>
-          </Link>
+          <button 
+            onClick={handleLogin}
+            disabled={loading}
+            className="bg-red-700 text-white w-full text-lg rounded-xl p-3 mt-2 mb-4 font-bold text-xl disabled:opacity-50"
+          >
+            {loading ? 'Logging in...' : 'Log in'}
+          </button>
 
           <div className=" text-[16px]">
             <p>
